b3cfc1bf8ed6739b0d1a4b32067f321250b22ac9
Christina Wang <cywang117@outlook.com>
2020-06-02 14:38:15 +0900

    Setup config and dependencies

---
+++ challenge_3/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, "client", "app.jsx"),
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "app.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, "client")
+        ],
+        loader: "babel-loader"
+      }
+    ]
+  },
+  devtool: "source-map"
+}
