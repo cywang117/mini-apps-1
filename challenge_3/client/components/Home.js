a781b14e65f9abca6291d335975733478c9c5226
Christina Wang <cywang117@outlook.com>
2020-06-03 10:45:15 +0900

    Complete home page

--- challenge_3/client/components/Home.js
+++ challenge_3/client/components/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Typography, Box, Button, Link } from '@material-ui/core';
+import { Link as RouterLink } from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <h1>Home page with button to checkout</h1>
-  );
-};
+const Home = () => (
+  <div className='centeredFlexBox'>
+    <Typography variant="h3" gutterBottom={true} align="center">Checkout Process</Typography>
+    <Typography variant="h6" gutterBottom={true} align="center">MERN Stack Demo</Typography>
+    <Box mx="auto" mt={5}>
+      <Button variant="contained" color="primary">
+        <Link component={RouterLink} to="/checkout/account" className="white">
+          Checkout &gt;
+        </Link>
+      </Button>
+    </Box>
+  </div>
+);
 
 export default Home;
