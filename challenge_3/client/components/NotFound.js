d53f0653399b00fee64a8546a6749ea03bf3c1cb
Christina Wang <cywang117@outlook.com>
2020-06-03 10:44:09 +0900

    Complete Not Found page

--- challenge_3/client/components/NotFound.js
+++ challenge_3/client/components/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
 
-const NotFound = () => {
-  return (
-    <div>404 Not Found</div>
-  )
-};
+const NotFound = () => (
+  <div className='centeredFlexBox'>
+    <Typography variant="h3" gutterBottom={true} align="center">Oops!</Typography>
+    <Typography align="center">The page you're looking for doesn't exist.</Typography>
+  </div>
+);
 
 export default NotFound;
