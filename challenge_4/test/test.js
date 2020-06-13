3a189b85c0bf7a2954b3a25167948ffaad848714
Christina Wang <cywang117@outlook.com>
2020-06-13 16:15:32 +0900

    Finish writing DOM / integration tests

--- challenge_4/test/test.js
+++ challenge_4/test/test.js
@@ -1,91 +1,15 @@
 import test from 'ava';
 import React from 'react';
-import { render, screen, cleanup } from '@testing-library/react';
+import { render, screen, cleanup, fireEvent } from '@testing-library/react';
 import App from '../client/src/components/App';
 
-let initialBoard = new Array(6).fill(new Array(7).fill('transparent'));
-let winningRows = [
-  [[0, 0], [0, 1], [0, 2], [0, 3]],
-  [[0, 1], [0, 2], [0, 3], [0, 4]],
-  [[0, 2], [0, 3], [0, 4], [0, 5]],
-  [[0, 3], [0, 4], [0, 5], [0, 6]],
-  [[1, 0], [1, 1], [1, 2], [1, 3]],
-  [[1, 1], [1, 2], [1, 3], [1, 4]],
-  [[1, 2], [1, 3], [1, 4], [1, 5]],
-  [[1, 3], [1, 4], [1, 5], [1, 6]],
-  [[2, 0], [2, 1], [2, 2], [2, 3]],
-  [[2, 1], [2, 2], [2, 3], [2, 4]],
-  [[2, 2], [2, 3], [2, 4], [2, 5]],
-  [[2, 3], [2, 4], [2, 5], [2, 6]],
-  [[3, 0], [3, 1], [3, 2], [3, 3]],
-  [[3, 1], [3, 2], [3, 3], [3, 4]],
-  [[3, 2], [3, 3], [3, 4], [3, 5]],
-  [[3, 3], [3, 4], [3, 5], [3, 6]],
-  [[4, 0], [4, 1], [4, 2], [4, 3]],
-  [[4, 1], [4, 2], [4, 3], [4, 4]],
-  [[4, 2], [4, 3], [4, 4], [4, 5]],
-  [[4, 3], [4, 4], [4, 5], [4, 6]],
-  [[5, 0], [5, 1], [5, 2], [5, 3]],
-  [[5, 1], [5, 2], [5, 3], [5, 4]],
-  [[5, 2], [5, 3], [5, 4], [5, 5]],
-  [[5, 3], [5, 4], [5, 5], [5, 6]]
-];
-let winningCols = [
-  [[0, 0], [1, 0], [2, 0], [3, 0]],
-  [[1, 0], [2, 0], [3, 0], [4, 0]],
-  [[2, 0], [3, 0], [4, 0], [5, 0]],
-  [[0, 1], [1, 1], [2, 1], [3, 1]],
-  [[1, 1], [2, 1], [3, 1], [4, 1]],
-  [[2, 1], [3, 1], [4, 1], [5, 1]],
-  [[0, 2], [1, 2], [2, 2], [3, 2]],
-  [[1, 2], [2, 2], [3, 2], [4, 2]],
-  [[2, 2], [3, 2], [4, 2], [5, 2]],
-  [[0, 3], [1, 3], [2, 3], [3, 3]],
-  [[1, 3], [2, 3], [3, 3], [4, 3]],
-  [[2, 3], [3, 3], [4, 3], [5, 3]],
-  [[0, 4], [1, 4], [2, 4], [3, 4]],
-  [[1, 4], [2, 4], [3, 4], [4, 4]],
-  [[2, 4], [3, 4], [4, 4], [5, 4]],
-  [[0, 5], [1, 5], [2, 5], [3, 5]],
-  [[1, 5], [2, 5], [3, 5], [4, 5]],
-  [[2, 5], [3, 5], [4, 5], [5, 5]],
-  [[0, 6], [1, 6], [2, 6], [3, 6]],
-  [[1, 6], [2, 6], [3, 6], [4, 6]],
-  [[2, 6], [3, 6], [4, 6], [5, 6]]
-];
-let winningMajorDiag = [
-  [[0, 0], [1, 1], [2, 2], [3, 3]],
-  [[0, 1], [1, 2], [2, 3], [3, 4]],
-  [[0, 2], [1, 3], [2, 4], [3, 5]],
-  [[0, 3], [1, 4], [2, 5], [3, 6]],
-  [[1, 0], [2, 1], [3, 2], [4, 3]],
-  [[1, 1], [2, 2], [3, 3], [4, 4]],
-  [[1, 2], [2, 3], [3, 4], [4, 5]],
-  [[1, 3], [2, 4], [3, 5], [4, 6]],
-  [[2, 0], [3, 1], [4, 2], [5, 3]],
-  [[2, 1], [3, 2], [4, 3], [5, 4]],
-  [[2, 2], [3, 3], [4, 4], [5, 5]],
-  [[2, 3], [3, 4], [4, 5], [5, 6]]
-];
-let winningMinorDiag = [
-  [[0, 3], [1, 2], [2, 1], [3, 0]],
-  [[0, 4], [1, 3], [2, 2], [3, 1]],
-  [[0, 5], [1, 4], [2, 3], [3, 2]],
-  [[0, 6], [1, 5], [2, 4], [3, 3]],
-  [[1, 3], [2, 2], [3, 1], [4, 0]],
-  [[1, 4], [2, 3], [3, 2], [4, 1]],
-  [[1, 5], [2, 4], [3, 3], [4, 2]],
-  [[1, 6], [2, 5], [3, 4], [4, 3]],
-  [[2, 3], [3, 2], [4, 1], [5, 0]],
-  [[2, 4], [3, 3], [4, 2], [5, 1]],
-  [[2, 5], [3, 4], [4, 3], [5, 2]],
-  [[2, 6], [3, 5], [4, 4], [5, 3]],
-];
+let playerColors = {
+  'Player 1': 'rgb(211, 52, 52)',
+  'Player 2': 'rgb(80, 80, 80)'
+};
 
-// React Testing Library (RTL)'s cleanup doesn't actually seem to work between test blocks
-// for now, so .before is used instead of .beforeEach, which will render App x times
-// (where x is the number of test blocks)
-test.before(() => render(<App />));
+// Run ava in serial mode (-s flag) to ensure .afterEach(cleanup) works as intended
+test.beforeEach(() => render(<App />));
 test.afterEach(cleanup);
 
 test('Smoke test - renders correctly', t => {
@@ -106,6 +30,14 @@ test('Smoke test - renders correctly', t => {
 
   // Game board
   t.notThrows(() => screen.getByTestId('game-board'));
+  // Placement row
+  try {
+    let placementRows = screen.getAllByTestId(/game-sq-p\d/);
+    t.notThrows(() => placementRows);
+    t.is(placementRows.length, 7);
+  } catch(e) {
+    t.fail(e.message);
+  }
   // Rows
   try {
     let rows = screen.getAllByTestId(/game-row-\d/);
@@ -129,18 +61,139 @@ test('Smoke test - renders correctly', t => {
   t.is(resetButtons.length, 2);
 });
 
-test('Updates board on clicking square', t => {
-  // TODO
-  t.fail();
+test('Updates even columns correctly on clicking placement button', t => {
+  // Piece should drop from highest slot to lowest free slot
+  for (let i = 0; i <= 6; i+=2) {
+    for (let j = 5; j >= 0; j--) {
+      fireEvent.click(screen.getByTestId(`game-sq-p${i}`), { button: 0 });
+      let style = window.getComputedStyle(screen.getByTestId(`game-sq-${j}${i}`));
+      t.is(style.background, playerColors[`Player ${j % 2 ? '1' : '2'}`]);
+    }
+  }
 });
 
-test('Updates displays appropriately only for valid wins', t => {
-  // TODO
-  t.fail();
+// Two separate test blocks to ensure board reset & prevent unintended win state (without testing reset button yet)
+test('Updates odd columns correctly on clicking placement button', t => {
+  // Piece should drop from highest slot to lowest free slot
+  for (let i = 1; i <= 5; i += 2) {
+    for (let j = 5; j >= 0; j--) {
+      fireEvent.click(screen.getByTestId(`game-sq-p${i}`), { button: 0 });
+      let style = window.getComputedStyle(screen.getByTestId(`game-sq-${j}${i}`));
+      t.is(style.background, playerColors[`Player ${j % 2 ? '1' : '2'}`]);
+    }
+  }
+});
+
+test('Resets board on clicking anywhere after win state', t => {
+  let cols = screen.getAllByTestId(/game-sq-p\d/);
+  let leftClick = { button: 0 };
+
+  const clickCol = (idx) => {
+    fireEvent.click(cols[idx], leftClick);
+  }
+
+  for (let i = 0; i < 4; i++) {
+    if (i !== 3) {
+      for (let j = 0; j < 2; j++) {
+        clickCol(j);
+      }
+    } else {
+      clickCol(0);
+    }
+  }
+  t.notThrows(() => screen.getByText(/wins/));
+  fireEvent.click(screen.getByText('Connect Four'), leftClick);
+  t.notThrows(() => screen.getByText('Player 1\'s turn!'));
 });
 
 test('Resets scores and/or board on button click', t => {
-  // TODO
-  t.fail();
+  let cols = screen.getAllByTestId(/game-sq-p\d/);
+  let leftClick = { button: 0 };
+
+  const clickCol = (idx) => {
+    fireEvent.click(cols[idx], leftClick);
+  }
+
+  // Reset Board button
+  clickCol(0);
+  clickCol(1);
+  t.is(window.getComputedStyle(screen.getByTestId('game-sq-50')).background, playerColors['Player 1']);
+  t.is(window.getComputedStyle(screen.getByTestId('game-sq-51')).background, playerColors['Player 2']);
+  fireEvent.click(screen.getByText('Reset Board'), leftClick);
+  t.is(window.getComputedStyle(screen.getByTestId('game-sq-50')).background, 'transparent');
+  t.is(window.getComputedStyle(screen.getByTestId('game-sq-51')).background, 'transparent');
+
+  // Reset Score button
+  for (let k = 0; k < 3; k++) {
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 2; j++) {
+        clickCol(i);
+      }
+    }
+  }
+  t.notThrows(() => screen.getByText('Player 1: 3'));
+  fireEvent.click(screen.getByText('Reset Scores'), leftClick);
+  t.notThrows(() => screen.getByText('Player 1: 0'));
 });
 
+test('Updates displays appropriately only for valid wins', t => {
+  let cols = screen.getAllByTestId(/game-sq-p\d/);
+  let leftClick = { button: 0 };
+
+  const clickCol = (idx) => {
+    fireEvent.click(cols[idx], leftClick);
+  }
+
+  // Row
+  for (let i = 0; i < 4; i++) {
+    if (i !== 3) {
+      for (let j = 0; j < 2; j++) {
+        clickCol(i);
+      }
+    } else {
+      clickCol(i);
+    }
+  }
+  t.notThrows(() => screen.getByText(/wins/));
+
+  // Col
+  fireEvent.click(screen.getByText('Reset Board'), leftClick);
+  for (let i = 0; i < 4; i++) {
+    if (i !== 3) {
+      for (let j = 0; j < 2; j++) {
+        clickCol(j);
+      }
+    } else {
+      clickCol(0);
+    }
+  }
+  t.notThrows(() => screen.getByText(/wins/));
+
+  // Major diag
+  fireEvent.click(screen.getByText('Reset Board'), leftClick);
+  clickCol(0);
+  for (let i = 0; i < 3; i++) {
+    clickCol(1);
+  }
+  for (let i = 0; i < 3; i++) {
+    clickCol(2);
+  }
+  for (let i = 0; i < 4; i++) {
+    clickCol(3);
+  }
+  t.notThrows(() => screen.getByText(/wins/));
+
+  // Minor diag
+  fireEvent.click(screen.getByText('Reset Board'), leftClick);
+  clickCol(6);
+  for (let i = 0; i < 3; i++) {
+    clickCol(5);
+  }
+  for (let i = 0; i < 3; i++) {
+    clickCol(4);
+  }
+  for (let i = 0; i < 4; i++) {
+    clickCol(3);
+  }
+  t.notThrows(() => screen.getByText(/wins/));
+});
